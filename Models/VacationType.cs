cb28a92450415d153d365dc3dc667890fd767943
bmiler <bianka.miler@gmail.com>
2018-11-05 21:21:46 +0100

    comment update

--- Models/VacationType.cs
+++ Models/VacationType.cs
@@ -2,9 +2,6 @@ using System.ComponentModel;
 
 namespace EmployeeVacationCalender.Models
 {
-    /// <summary>
-    ///     Types of vacation an employee can take
-    /// </summary>
     public enum VacationType
     {
         [DisplayName("Vacation Leave")]
